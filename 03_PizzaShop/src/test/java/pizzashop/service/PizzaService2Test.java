a980189fabf6a3823bb912668e582b4bf2591aba
Razvan Ispas <razvanip44@gmail.com>
2022-04-29 19:03:29 +0300

    Holy push

--- 03_PizzaShop/src/test/java/pizzashop/service/PizzaService2Test.java
+++ 03_PizzaShop/src/test/java/pizzashop/service/PizzaService2Test.java
@@ -17,7 +17,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-class PizzaService2 {
+class PizzaService2Test {
 
     private PaymentRepository paymentRepository;
     private MenuRepository menuRepository;
